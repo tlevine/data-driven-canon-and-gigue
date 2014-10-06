5aba959c23b937b5926fd1e65ea857f78d97dd14
Thomas Levine <_@thomaslevine.com>
2014-10-06 02:34:33 +0000

    -

--- src/score.hs
+++ src/score.hs
@@ -1,4 +1,9 @@
-type ViolinPhrase = LilyPondThingy
--- And check that it is either four bars or eight bars long.
+-- These are the violin I phrases;
+-- Cello line is constant, and violins II and III
+-- are inferred from violin I.
+type Score = [Phrase]
 
-type Score = [ViolinPhrase]
+data PhraseLength = PhraseLength Four | Eight
+type Phrase = { length :: PhraseLength
+              , variation :: ??? -- Quarter notes, eigth notes, leaping 32nd notes, &c.
+
